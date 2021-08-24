04cabfc7e2a6e532915b2f30f5bee13ecd1621ca
Andrey Denisov <telcontar2012@yandex.ru>
2021-08-25 00:09:42 +0500

    change js

--- scripts/submit.js
+++ scripts/submit.js
@@ -2,16 +2,17 @@ let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 
 let nameComponent = document.querySelector('.profile__title');
-let nameText = nameComponent.textContent;
 let nameField = document.getElementById('input_name');
 
 let jobComponent = document.querySelector('.profile__subtitle');
-let jobText = jobComponent.textContent;
+
 let jobField = document.getElementById('input_job');
 
 function openPopup () {
-  nameField.setAttribute('value', nameText); //textContent не работает в этом случае, так как нет закрывающегося тэга
-  jobField.setAttribute('value', jobText); //textContent не работает в этом случае, так как нет закрывающегося тэга
+  let nameText = nameComponent.textContent;
+  let jobText = jobComponent.textContent;
+  nameField.value = nameText; 
+  jobField.value = jobText;
   popup.classList.add('popup_opened');
 };
 openPopupButton.addEventListener('click', openPopup);

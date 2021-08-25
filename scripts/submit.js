7abf7bfde132b0a23e3f20496d80026634ff3bf6
Andrey Denisov <telcontar2012@yandex.ru>
2021-08-25 09:09:44 +0500

    change js

--- scripts/submit.js
+++ scripts/submit.js
@@ -2,17 +2,16 @@ let popup = document.querySelector('.popup');
 let openPopupButton = document.querySelector('.profile__edit-button');
 
 let nameComponent = document.querySelector('.profile__title');
+let nameText = nameComponent.textContent;
 let nameField = document.getElementById('input_name');
 
 let jobComponent = document.querySelector('.profile__subtitle');
-
+let jobText = jobComponent.textContent;
 let jobField = document.getElementById('input_job');
 
 function openPopup () {
-  let nameText = nameComponent.textContent;
-  let jobText = jobComponent.textContent;
-  nameField.value = nameText; 
-  jobField.value = jobText;
+  nameField.textContent = nameText; 
+  jobField.textContent = jobText;
   popup.classList.add('popup_opened');
 };
 openPopupButton.addEventListener('click', openPopup);
